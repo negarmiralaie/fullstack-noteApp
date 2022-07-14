3643653abf0518551dea1ed5afa738eb1781b746
negar miralaie <miralaienegar@gmail.com>
2022-07-14 05:40:37 +0430

    added config

---
+++ server/config/customEnvironmentVariables.ts
@@ -0,0 +1,4 @@
+export default {
+    port: 'PORT',
+    dbUri: 'DB_CONNECTION',
+}
---
+++ server/config/default.ts
@@ -0,0 +1,4 @@
+export default {
+    port: 1337,
+    dbUri: 'mongodb+srv://negarm:negarm@cluster0.ioovc.mongodb.net/notepad?retryWrites=true&w=majority',
+}
